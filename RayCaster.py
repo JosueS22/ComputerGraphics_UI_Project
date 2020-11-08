c2abdac3af21eba490c80901640b2a011462d891
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-11-07 18:23:14 -0600

    sprites

--- RayCaster.py
+++ RayCaster.py
@@ -1,127 +1,22 @@
 import pygame
-from math import cos, sin, pi
+from gl import *
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-BACKGROUND = (64,64,64)
-GREEN = (152,251,152)
+pygame.init()
+screen = pygame.display.set_mode((1000,500), pygame.DOUBLEBUF | pygame.HWACCEL) #, pygame.FULLSCREEN)
+screen.set_alpha(None)
+clock = pygame.time.Clock()
 
-textures = {
-    '1' : pygame.image.load('./images/diamond.png'),
-    '2' : pygame.image.load('./images/stone.png'),
-    '3' : pygame.image.load('./images/emerald.png')
-    }
+width = screen.get_width()
+height = screen.get_height()
 
-class Raycaster(object):
-    def __init__(self,screen):
-        self.screen = screen
-        _, _, self.width, self.height = screen.get_rect()
-
-        self.map = []
-        self.blocksize = 25
-        self.wallHeight = 25
-
-        self.stepSize = 5
-
-        self.player = {
-            "x" : 65,
-            "y" : 50,
-            "angle" : 90,
-            "fov" : 40
-            }
-
-    def load_map(self, filename):
-        with open(filename) as f:
-            for line in f.readlines():
-                self.map.append(list(line))
-
-    def drawRect(self, x, y, tex):
-        tex = pygame.transform.scale(tex, (self.blocksize, self.blocksize))
-        rect = tex.get_rect()
-        rect = rect.move( (x,y) )
-        self.screen.blit(tex, rect)
-
-    def drawPlayerIcon(self,color):
-
-        rect = (int(self.player['x'] - 2), int(self.player['y'] - 2), 5, 5)
-        self.screen.fill(color, rect)
-
-    def castRay(self, a):
-        rads = a * pi / 180
-        dist = 0
-        while True:
-            x = int(self.player['x'] + dist * cos(rads))
-            y = int(self.player['y'] + dist * sin(rads))
-
-            i = int(x/self.blocksize)
-            j = int(y/self.blocksize)
-
-            if self.map[j][i] != ' ':
-                hitX = x - i*self.blocksize
-                hitY = y - j*self.blocksize
-
-                if 1 < hitX < self.blocksize - 1:
-                    maxHit = hitX
-                else:
-                    maxHit = hitY
-
-                tx = maxHit / self.blocksize
-
-                return dist, self.map[j][i], tx
-
-            self.screen.set_at((x,y), WHITE)
-
-            dist += 5
-
-    def render(self):
-        halfWidth = int(self.width / 2)
-        halfHeight = int(self.height / 2)
-
-        for x in range(0, halfWidth, self.blocksize):
-            for y in range(0, self.height, self.blocksize):
-                
-                i = int(x/self.blocksize)
-                j = int(y/self.blocksize)
-
-                if self.map[j][i] != ' ':
-                    self.drawRect(x, y, textures[self.map[j][i]])
-
-        self.drawPlayerIcon(BLACK)
-
-        for i in range(halfWidth):
-            angle = self.player['angle'] - self.player['fov'] / 2 + self.player['fov'] * i / halfWidth
-            dist, wallType, tx = self.castRay(angle)
-
-            x = halfWidth + i 
-
-            # perceivedHeight = screenHeight / (distance * cos( rayAngle - viewAngle) * wallHeight
-            h = self.height / (dist * cos( (angle - self.player['angle']) * pi / 180 )) * self.wallHeight
-
-            start = int( halfHeight - h/2)
-            end = int( halfHeight + h/2)
-
-            img = textures[wallType]
-            tx = int(tx * img.get_width())
-
-            for y in range(start, end):
-                ty = (y - start) / (end - start)
-                ty = int(ty * img.get_height())
-                texColor = img.get_at((tx, ty))
-                self.screen.set_at((x, y), texColor)
-
-        for i in range(self.height):
-            self.screen.set_at( (halfWidth, i), BLACK)
-            self.screen.set_at( (halfWidth+1, i), BLACK)
-            self.screen.set_at( (halfWidth-1, i), BLACK)
-
-    def message_to_screen(self, msg, color, x, y, size = 35, font = 0):
+def message_to_screen(msg, color, x, y, size = 35, font = 0):
         font0 = pygame.font.SysFont('Corbel', size)
         font1 = pygame.font.SysFont('Arial', size)
         fonts = [font0, font1]
         screen_text = fonts[font].render(msg, True, color)
         screen.blit(screen_text, (x,y))
 
-    def pause(self):
+def pause():
 
         paused = True
         while paused:
@@ -147,10 +42,10 @@ class Raycaster(object):
             continue_text = 'Continue'
             quit_text = 'Quit'
 
-            self.message_to_screen(pause_text, WHITE, width/2 - 45, height/4, 50)
-            self.message_to_screen(pause2_text, WHITE, width/2 -250, height/4 + 60, 25)
-            self.message_to_screen(continue_text, WHITE, width/2 - 50, height/2)
-            self.message_to_screen(quit_text, WHITE, width/2 - 20, height/2 + 50)
+            message_to_screen(pause_text, WHITE, width/2 - 45, height/4, 50)
+            message_to_screen(pause2_text, WHITE, width/2 -250, height/4 + 60, 25)
+            message_to_screen(continue_text, WHITE, width/2 - 50, height/2)
+            message_to_screen(quit_text, WHITE, width/2 - 20, height/2 + 50)
             
             pygame.display.update()
 
@@ -173,15 +68,6 @@ class Raycaster(object):
 
             clock.tick(30)
 
-
-pygame.init()
-screen = pygame.display.set_mode((1000,500), pygame.DOUBLEBUF | pygame.HWACCEL) #, pygame.FULLSCREEN)
-screen.set_alpha(None)
-clock = pygame.time.Clock()
-
-width = screen.get_width()
-height = screen.get_height()
-
 def updateFPS():
     fps = str(int(clock.get_fps()))
     fps = pygame.font.SysFont('Corbel', 35).render(fps, 1, pygame.Color("white"))
@@ -201,10 +87,10 @@ def gameLoop():
             menu_text2 = 'Presione START para iniciar o QUIT para salir.'
             start_text = 'Start'
             quit_text = 'Quit'
-            r.message_to_screen(menu_text, WHITE, width/2-40, height/2-100)
-            r.message_to_screen(menu_text2, WHITE, width/2 - 200, height/2 - 50, 25)
-            r.message_to_screen(start_text, WHITE, width/2, height/2)
-            r.message_to_screen(quit_text, WHITE, width/2, height/2+50)
+            message_to_screen(menu_text, WHITE, width/2-40, height/2-100)
+            message_to_screen(menu_text2, WHITE, width/2 - 200, height/2 - 50, 25)
+            message_to_screen(start_text, WHITE, width/2, height/2)
+            message_to_screen(quit_text, WHITE, width/2, height/2+50)
 
             pygame.display.update()
             
@@ -271,7 +157,7 @@ def gameLoop():
                 elif ev.key == pygame.K_e:
                     r.player['angle'] += 5
                 elif ev.key == pygame.K_p:
-                    r.pause()
+                    pause()
 
                 i = int(newX / r.blocksize)
                 j = int(newY / r.blocksize)
@@ -289,7 +175,7 @@ def gameLoop():
 
         # Main menu pause
         screen.fill(pygame.Color("black"), (880,0,120,30))
-        r.message_to_screen('p to pause',WHITE,880,0,25)
+        message_to_screen('p to pause',WHITE,880,0,25)
 
         clock.tick(30)  
         pygame.display.update()
